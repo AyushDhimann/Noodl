b7703ed392d73ac4e90f5ff1eaef01be39061a08
Parth Kalia <95091403+TheParthK@users.noreply.github.com>
2025-06-09 02:03:31 +0530

    BUGS BUGS BUGS

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:frontend/pages/home.dart';
 import 'package:frontend/pages/login.dart';
-import 'package:frontend/pages/metamasktest.dart';
+import 'package:frontend/test/metamasktest.dart';
 import 'package:frontend/pages/quiz.dart';
 import 'package:frontend/providers/quiz_page_provider.dart';
-import 'package:frontend/services/metamask_service.dart';
+import 'package:frontend/test/metamask_service.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+
   runApp(const MyApp());
 }
 
@@ -28,10 +30,20 @@ class MyApp extends StatelessWidget {
           useMaterial3: true
         ),
         // ignore: prefer_const_constructors
-        // home: QuizPage(),
+        home: QuizPage(),
+        // home: HomePage(),
         // ignore: prefer_const_constructors
-        // home: LoginPage(),
-        home: WalletConnector(),
+      //   home: StreamBuilder(
+      //   stream: FirebaseAuth.instance.authStateChanges(),
+      //   builder: (context, snapshot) {
+      //     if(snapshot.hasData){
+      //       return const QuizPage();
+      //     } else {
+      //       return const LoginPage();
+      //     }
+      //   },
+      // ),
+        // home: HomePage(),
       ),
     );
   }
